92a46046dd7feb1f7015bfbe8bf9f63e3f1f54f9
jaddu17 <shafajaddu@gmail.com>
2025-12-19 11:54:09 +0700

    menambahkan interface RepositoryDataSiswa

---
+++ app/src/main/java/com/example/pertemuan12/repositori/RepositoryDataSiswa.kt
@@ -0,0 +1,16 @@
+package com.example.pertemuan12.repositori
+
+import com.example.pertemuan12.apiservice.ServiceApiSiswa
+import com.example.pertemuan12.modeldata.DataSiswa
+
+interface RepositoryDataSiswa{
+    suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
