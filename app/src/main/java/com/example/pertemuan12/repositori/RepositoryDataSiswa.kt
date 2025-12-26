4ec72f48d558663c825dd931877d52a32bd71c7e
jaddu17 <shafajaddu@gmail.com>
2025-12-26 15:57:43 +0700

    menambahkan fun update

--- app/src/main/java/com/example/pertemuan12/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/pertemuan12/repositori/RepositoryDataSiswa.kt
@@ -2,10 +2,14 @@ package com.example.pertemuan12.repositori
 
 import com.example.pertemuan12.apiservice.ServiceApiSiswa
 import com.example.pertemuan12.modeldata.DataSiswa
+import retrofit2.Response
 
 interface RepositoryDataSiswa{
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa):retrofit2.Response<Void>
+
 }
 
 class JaringanRepositoryDataSiswa(
@@ -13,4 +17,6 @@ class JaringanRepositoryDataSiswa(
 ): RepositoryDataSiswa{
     override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.editSatuSiswa(id, dataSiswa)
 }
