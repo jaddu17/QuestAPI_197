5e2ee199e83d61b067d82af9ec4ea3fc83c972da
jaddu17 <shafajaddu@gmail.com>
2025-12-26 15:56:13 +0700

    menambahkan fun untuk bacateman1

--- app/src/main/java/com/example/pertemuan12/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/pertemuan12/apiservice/ServiceApiSiswa.kt
@@ -2,8 +2,12 @@ package com.example.pertemuan12.apiservice
 
 import com.example.pertemuan12.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
 
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
@@ -11,4 +15,9 @@ interface ServiceApiSiswa {
 
     @POST("insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    @GET ("baca1Teman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id : Int) : DataSiswa
+
+
 }
