753e93b91fbef170aaa548ed29e129f851e0823e
jaddu17 <shafajaddu@gmail.com>
2025-12-26 16:06:51 +0700

    menambahkan fun itemdetails

--- app/src/main/java/com/example/pertemuan12/view/HalamanDetail.kt
+++ app/src/main/java/com/example/pertemuan12/view/HalamanDetail.kt
@@ -139,3 +139,39 @@ private fun ItemDetailsBody(
     }
 }
 
+@Composable
+fun ItemDetails(
+    siswa: DataSiswa,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier, colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+        ) {
+            ItemDetailRow(
+                labelResID = R.string.nama,
+                itemDetail = siswa.nama,
+                modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_medium))
+            )
+            ItemDetailRow(
+                labelResID = R.string.alamat,
+                itemDetail = siswa.alamat,
+                modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_medium))
+            )
+            ItemDetailRow(
+                labelResID = R.string.telpon,
+                itemDetail = siswa.telpon,
+                modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+    }
+}
+
