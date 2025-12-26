5253d2919ee1d04698ad44ba649709b4efce1c5d
jaddu17 <shafajaddu@gmail.com>
2025-12-26 16:34:29 +0700

    memperbaiki halamanhome

--- app/src/main/java/com/example/pertemuan12/view/HalamanHome.kt
+++ app/src/main/java/com/example/pertemuan12/view/HalamanHome.kt
@@ -1,11 +1,9 @@
 package com.example.pertemuan12.view
 
-import androidx.compose.foundation.Image
-import com.example.pertemuan12.modeldata.DataSiswa
+ import com.example.pertemuan12.modeldata.DataSiswa
 import com.example.pertemuan12.viewmodel.HomeViewModel
 import com.example.pertemuan12.viewmodel.StatusUiSiswa
 import com.example.pertemuan12.viewmodel.provider.PenyediaViewModel
-
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -17,12 +15,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Phone
-
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -35,19 +30,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan12.R
-import com.example.pertemuan12.modeldata.UIStateSiswa
 import com.example.pertemuan12.uicontroller.route.DestinasiHome
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
